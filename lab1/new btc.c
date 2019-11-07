e4887b3365fb444dc3185aa4b73e855f19896dd6
Greench1108 <1839848600@qq.com>
2019-11-07 07:52:32 -0800

    added a new btc.c file

---
+++ lab1/new btc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int c,a;
+    int n=0;
+    int b;
+    c=getchar();
+    while(c!=EOF){
+        c=getchar();
+    }
+    a="c"-"0";
+    if(a>1){
+       if(b=a/2)
+        a=b;
+        b=b/2;}
+    else;
+        n=n+1;
+        a=(a-1)/2;
+    }
+    printf("%d\n",int n);
+}
