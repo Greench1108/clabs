2b2854ef58842e4b65405f705b1d9d1ef247177d
Greench1108 <1839848600@qq.com>
2019-11-14 21:47:49 -0800

    added a btc.c file

--- lab1/btc.c
+++ lab1/btc.c
@@ -2,14 +2,14 @@
  int main()
 {
     int n=0;
-    int n1=0;
+    int x=0;
     int n2=0;
     scanf("%d",&n2);
     while(n2!=0)
     {
-        n1=n2%2;
+        x=n2%2;
         n2=n2/2;
-        if(n1==1)
+        if(x==1)
         n=n+1;
     }
     printf("%d\n",n);
